0ae994559edc2354b173ea658514c634913dd342
Vinicius Uzeda <vruzeda@gmail.com>
2016-11-22 21:30:06 -0200

    Importing barcode reader activity.

--- app/src/main/java/com/upversionlab/veganapp/MainActivity.java
+++ app/src/main/java/com/upversionlab/veganapp/MainActivity.java
@@ -1,13 +1,50 @@
 package com.upversionlab.veganapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.samples.vision.barcodereader.BarcodeCaptureActivity;
+import com.google.android.gms.vision.barcode.Barcode;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_BARCODE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View barcodeButton = findViewById(R.id.activity_main_barcode_button);
+        barcodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // launch barcode activity.
+                Intent intent = new Intent(MainActivity.this, BarcodeCaptureActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_BARCODE_CAPTURE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_BARCODE_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    Toast.makeText(this, barcode.displayValue, Toast.LENGTH_SHORT).show();
+                } else {
+                    // TODO Handle error
+                }
+            } else {
+                // TODO Handle error
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
