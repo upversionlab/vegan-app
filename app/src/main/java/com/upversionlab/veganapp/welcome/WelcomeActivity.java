5bcb1dbb2b035c8d02b78279d6aa6911f9eafc18
Vinicius Uzeda <vruzeda@gmail.com>
2016-12-29 00:12:29 -0200

    Setting WelcomeActivity as the app's entry point, and linking it to the MainActivity.

--- app/src/main/java/com/upversionlab/veganapp/welcome/WelcomeActivity.java
+++ app/src/main/java/com/upversionlab/veganapp/welcome/WelcomeActivity.java
@@ -1,10 +1,12 @@
 package com.upversionlab.veganapp.welcome;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.upversionlab.veganapp.MainActivity;
 import com.upversionlab.veganapp.R;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -15,7 +17,7 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        Button next = (Button) findViewById(R.id.activity_welcome_next);
+        View next = findViewById(R.id.activity_welcome_next);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,12 +26,22 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         });
 
-        Button back = (Button) findViewById(R.id.activity_welcome_back);
+        View back = findViewById(R.id.activity_welcome_back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 currentStep--;
                 screensHandler(currentStep);
+            }
+        });
+
+        View start = findViewById(R.id.activity_welcome_start_button);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
